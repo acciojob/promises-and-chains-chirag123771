de58c5b7052f7ce2f7cc183e40c7873a23422c0f
Chirag Agarwal <72219269+chirag123771@users.noreply.github.com>
2024-02-05 01:59:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+ document.getElementById('myForm').addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      const ageInput = document.getElementById('age');
+      const nameInput = document.getElementById('name');
+
+      if (!ageInput.value || !nameInput.value) {
+        alert('Both age and name are required');
+        return;
+      }
+
+      const age = parseInt(ageInput.value);
+
+      const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (age >= 18) {
+            resolve(`Welcome, ${nameInput.value}. You can vote.`);
+          } else {
+            reject(`Oh sorry ${nameInput.value}. You aren't old enough.`);
+          }
+        }, 4000);
+      });
+
+      promise.then((message) => {
+        alert(message);
+      }).catch((message) => {
+        alert(message);
+      });
+    });
